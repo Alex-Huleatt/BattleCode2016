50875af335d0bc0503b61a5d91f2b3c84bcc3f5c
Todd Beckman <toddbeckman1@gmail.com>
2016-01-06 00:02:46 -0700

    made a very basic archon

--- units/archon/ArchonDefault.java
+++ units/archon/ArchonDefault.java
@@ -1,29 +1,83 @@
 package team018.units.archon;
 
+import java.util.Random;
+
+import battlecode.common.Direction;
 import battlecode.common.RobotController;
+import battlecode.common.RobotType;
 import team018.frameworks.moods.Mood;
 
 /**
  * Created by alexhuleatt on 1/4/16.
+ * 
+ * Update by Todd: Attempts to spawn a random unit based on a simple probability check.
+ * Should spawn in a random direction.
  */
 public class ArchonDefault extends Mood {
-
+	Random rand;
+	static final Direction[] directions = Direction.values();
+	
+	//	The highest-to-lowest preferences of robots to build
+	static final RobotType[] robotPriorities = {
+			RobotType.SCOUT,
+			RobotType.GUARD,
+			RobotType.VIPER,
+			RobotType.TURRET,
+			RobotType.SOLDIER
+	};
+	//	The likeliness of building each robot type out of 128
+	static final int[] priorityLevels = {
+			50,
+			50,
+			10,
+			10,
+			8
+	};
+	RobotType lastSpawned, toSpawn;
 
     public ArchonDefault(RobotController rc) {
         super(rc);
-
+        rand = new Random(rc.getID());
     }
 
     @Override
     public void update() {
         super.update();
-
-
+        
+        //	Only prepare for building if it can build
+        if (rc.isCoreReady())
+        {
+	        lastSpawned = toSpawn;
+	        
+	        //	Determine where to spawn next
+	        int next = rand.nextInt() & 127,
+	        	i = 0;
+	        while (-1 < next)
+	        {
+		        if (next < priorityLevels[i])
+		        {
+		        	toSpawn = robotPriorities[i];
+		        	break;
+		        }
+		        next -= priorityLevels[i++];
+	        }
+        }
     }
 
     @Override
-    public void act() {
-
+    public void act() throws Exception {
+    	if (rc.isCoreReady())
+    	{
+    		//	Build. Goes in order of direction looking for place to build
+    		for (Direction d: directions)
+    		{
+	    		if (rc.canBuild(d, toSpawn))
+	    		{
+	    			rc.build(d, toSpawn);
+	    			break;
+	    		}
+    		}
+    	}
     }
 
     @Override
