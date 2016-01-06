9ec9d3e2a95bf5bff15edae276b65194be579afc
Todd Beckman <toddbeckman1@gmail.com>
2016-01-06 10:25:43 -0700

    won the battle against the tabs

--- units/archon/ArchonDefault.java
+++ units/archon/ArchonDefault.java
@@ -9,82 +9,87 @@ import team018.frameworks.moods.Mood;
 
 /**
  * Created by alexhuleatt on 1/4/16.
- * 
+ * <p>
  * Update by Todd: Attempts to spawn a random unit based on a simple probability check.
  * Should spawn in a random direction.
  */
-public class ArchonDefault extends Mood {
-	Random rand;
-	static final Direction[] directions = Direction.values();
-	
-	//	The highest-to-lowest preferences of robots to build
-	static final RobotType[] robotPriorities = {
-			RobotType.SCOUT,
-			RobotType.GUARD,
-			RobotType.VIPER,
-			RobotType.TURRET,
-			RobotType.SOLDIER
-	};
-	//	The likeliness of building each robot type out of 128
-	static final int[] priorityLevels = {
-			0,
-			128,
-			10,
-			10,
-			8
-	};
-	RobotType lastSpawned, toSpawn;
-	
-	boolean ready;
+public class ArchonDefault extends Mood
+{
+    Random rand;
+    static final Direction[] directions = Direction.values();
 
-    public ArchonDefault(RobotController rc) {
+    //	The highest-to-lowest preferences of robots to build
+    static final RobotType[] robotPriorities = {
+            RobotType.SCOUT,
+            RobotType.GUARD,
+            RobotType.VIPER,
+            RobotType.TURRET,
+            RobotType.SOLDIER
+    };
+    //	The likeliness of building each robot type out of 128
+    static final int[] priorityLevels = {
+            0,
+            128,
+            10,
+            10,
+            8
+    };
+    RobotType lastSpawned, toSpawn;
+
+    boolean ready;
+
+    public ArchonDefault(RobotController rc)
+    {
         super(rc);
         rand = new Random(rc.getID());
     }
 
     @Override
-    public void update() {
+    public void update()
+    {
         super.update();
-        
+
         ready = rc.isCoreReady();
     }
 
     @Override
-    public void act() throws Exception {
-    	if (ready)
-    	{
-    		
-    		
-        	//Building logic
-	        lastSpawned = toSpawn;
-	        
-	        //	Determine where to spawn next
-	        int next = rand.nextInt() & 127,
-	        	i = 0;
-	        while (-1 < next)
-	        {
-		        if (next < priorityLevels[i])
-		        {
-		        	toSpawn = robotPriorities[i];
-		        	break;
-		        }
-		        next -= priorityLevels[i++];
-	        }
-	        
-    		//	Build. Goes in order of direction looking for place to build
-    		for (Direction d: directions)
-    		{
-	    		if (rc.hasBuildRequirements(toSpawn) && rc.canBuild(d, toSpawn))
-	    		{
-	    			rc.build(d, toSpawn);
-	    			break;
-	    		}
-    		}
-    	}
+    public void act() throws Exception
+    {
+        if (ready)
+        {
+
+
+            //Building logic
+            lastSpawned = toSpawn;
+
+            //	Determine where to spawn next
+            int next = rand.nextInt() & 127,
+                    i = 0;
+            while (-1 < next)
+            {
+                if (next < priorityLevels[i])
+                {
+                    toSpawn = robotPriorities[i];
+                    break;
+                }
+                next -= priorityLevels[i++];
+            }
+
+            //	Build. Goes in order of direction looking for place to build
+            for (Direction d : directions)
+            {
+                if (rc.hasBuildRequirements(toSpawn) && rc.canBuild(d, toSpawn))
+                {
+                    rc.build(d, toSpawn);
+                    break;
+                }
+            }
+        }
     }
 
     @Override
-    public Mood swing() {
+    public Mood swing()
+    {
         return null;
     }
 }
--- units/guard/GuardDefault.java
+++ units/guard/GuardDefault.java
@@ -12,74 +12,76 @@ import team018.frameworks.movement.MovementController;
 /**
  * Created by alexhuleatt on 1/4/16.
  */
-public class GuardDefault extends Mood {
-	MapLocation myLocation;
-	int attackRangeSquared, sensorRangeSquared;
-	MovementController mc;
-	
-	
-    public GuardDefault(RobotController rc) {
+public class GuardDefault extends Mood
+{
+    MapLocation myLocation;
+    int attackRangeSquared, sensorRangeSquared;
+    MovementController mc;
+
+    public GuardDefault(RobotController rc)
+    {
         super(rc);
         attackRangeSquared = RobotType.GUARD.attackRadiusSquared;
         sensorRangeSquared = RobotType.GUARD.sensorRadiusSquared;
         mc = new MovementController(rc);
     }
-    
+
     public void update()
     {
-    	myLocation = rc.getLocation();
+        myLocation = rc.getLocation();
     }
 
     @Override
-    public void act() throws Exception {
-    	
-    	//	If it can attack, try!
-    	if (rc.isCoreReady())
-    	{
-        	RobotInfo[] robots = rc.senseHostileRobots(myLocation, sensorRangeSquared);
-        	rc.setIndicatorString(0, "Robots: " + robots.length);
-        	
-        	RobotInfo closest = null;
-        	MapLocation closestLocation = null, checkLocation;
-        	int distance = Integer.MAX_VALUE, checkDistance, x, y;
-        	for (int i = 0; i < robots.length; i++)
-        	{
-        		checkLocation = robots[i].location;
-        		x = myLocation.x - checkLocation.x;
-        		y = myLocation.y - checkLocation.y;
-        		checkDistance = x * x + y * y;
-        		if (checkDistance < distance)
-        		{
-        			closest = robots[i];
-        			closestLocation = checkLocation;
-        			distance = checkDistance;
-        		}
-        	}
-        	//	Make sure it found something
-        	if (closest != null)
-        	{
-        		rc.setIndicatorString(1, "Targetting " + closest.location.toString());
-        		if (rc.canAttackLocation(closestLocation))
-        		{
-        			rc.attackLocation(closestLocation);
-        		}
-        		else
-        		{
-        			Direction direction = myLocation.directionTo(closestLocation);
-        			if (rc.canMove(direction))
-        			{
-        				rc.move(direction);
-        			}
-        			else
-        			{
-            			MapLocation destination = myLocation.add(direction);
-        				if (GameConstants.RUBBLE_OBSTRUCTION_THRESH < rc.senseRubble(destination))
-        				{
-        					rc.clearRubble(direction);
-        				}
-        			}
-        		}
-        	}
-    	}
+    public void act() throws Exception
+    {
+
+        // If it can attack, try!
+        if (rc.isCoreReady())
+        {
+            RobotInfo[] robots = rc.senseHostileRobots(myLocation, sensorRangeSquared);
+            rc.setIndicatorString(0, "Robots: " + robots.length);
+
+            RobotInfo closest = null;
+            MapLocation closestLocation = null, checkLocation;
+            int distance = Integer.MAX_VALUE, checkDistance, x, y;
+            for (int i = 0; i < robots.length; i++)
+            {
+                checkLocation = robots[i].location;
+                x = myLocation.x - checkLocation.x;
+                y = myLocation.y - checkLocation.y;
+                checkDistance = x * x + y * y;
+                if (checkDistance < distance)
+                {
+                    closest = robots[i];
+                    closestLocation = checkLocation;
+                    distance = checkDistance;
+                }
+            }
+            // Make sure it found something
+            if (closest != null)
+            {
+                rc.setIndicatorString(1, "Targetting " + closest.location.toString());
+                if (rc.canAttackLocation(closestLocation))
+                {
+                    rc.attackLocation(closestLocation);
+                }
+                else
+                {
+                    Direction direction = myLocation.directionTo(closestLocation);
+                    if (rc.canMove(direction))
+                    {
+                        rc.move(direction);
+                    }
+                    else
+                    {
+                        MapLocation destination = myLocation.add(direction);
+                        if (GameConstants.RUBBLE_OBSTRUCTION_THRESH < rc.senseRubble(destination))
+                        {
+                            rc.clearRubble(direction);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
