f9e6b5eaa2c4861653d559b268a93a82f58023ef
Todd Beckman <toddbeckman1@gmail.com>
2016-01-13 22:53:20 -0700

    more turret logic progress

--- units/turret/SoloTurret.java
+++ units/turret/SoloTurret.java
@@ -39,7 +39,8 @@ public class SoloTurret extends Mood
 
         while ((si = c.receiveSignal()) != null) {
 
-            if (si.type == SignalType.ATTACK && si.data < 6)
+            if (si.type == SignalType.ATTACK || si.data < TeamTurret.TIMEOUT + rc.getRoundNum()
+                    && me.distanceSquaredTo(si.targetLoc) <= RobotType.TURRET.attackRadiusSquared)
             {
                 System.out.println("hi");
                 target = si.targetLoc;
